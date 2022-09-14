2e61cc2a514e98e9fead9b692dc2b6ea4e90450d
Shazam213 <103990753+Shazam213@users.noreply.github.com>
2022-09-15 03:48:24 +0530

    Add files via upload

---
+++ mls/build/CMakeFiles/resampling.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/resampling.dir/MLS.cpp.o"
+  "resampling"
+  "resampling.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/resampling.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
